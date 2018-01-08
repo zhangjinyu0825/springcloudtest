31d48415b7e789f026e05696379f44f6c2835a06
zhangjinyu0825 <13261476221@163.com>
2018-01-08 18:00:25 +0800

    修改

--- config-client/src/main/java/com/cml/springcloud/ConfigClientApplication.java
+++ config-client/src/main/java/com/cml/springcloud/ConfigClientApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RefreshScope
 public class ConfigClientApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ConfigClientApplication.class, args);
