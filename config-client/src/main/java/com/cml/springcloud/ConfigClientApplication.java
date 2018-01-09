b2cff565bbc16e2b1173ff38a9673aefccf031ff
zhangjinyu0825 <13261476221@163.com>
2018-01-09 16:52:50 +0800

    03

--- config-client/src/main/java/com/cml/springcloud/ConfigClientApplication.java
+++ config-client/src/main/java/com/cml/springcloud/ConfigClientApplication.java
@@ -2,12 +2,10 @@ package com.cml.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RefreshScope
 public class ConfigClientApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ConfigClientApplication.class, args);
