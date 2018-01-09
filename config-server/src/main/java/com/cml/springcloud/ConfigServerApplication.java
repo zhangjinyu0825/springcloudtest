f4f2855457e7d3e7048f03053eef25b29af58b03
zhangjinyu0825 <13261476221@163.com>
2018-01-09 10:40:11 +0800

    测试1

--- config-server/src/main/java/com/cml/springcloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/cml/springcloud/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.cml.springcloud;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigServerApplication {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
